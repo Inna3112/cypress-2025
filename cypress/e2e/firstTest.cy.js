24b58bb13bb04dffd225a6f5bd690dd8ad4afefc
Inna3112 <innula3112@gmail.com>
2025-12-12 15:11:54 +0200

    extracting values

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -83,7 +83,7 @@ describe('Test Suite 1', () => {
       .should('have.text', 'Option 1')
   })
 
-  it.only('Reusing Locators', () => {
+  it('Reusing Locators', () => {
 
     //THIS WILL NOT WORK!!! DON"T DO LIKE THIS!!!
     //const inputEmail1 = cy.get('#inputEmail1')
@@ -107,6 +107,33 @@ describe('Test Suite 1', () => {
 
     cy.get('@inputEmail2').click()
 
+  })
+
+  it.only('Extracting Values', () => {
+    // 1. using a JQuery method
+    cy.get('[for="exampleInputEmail1"]').then( label => {
+      const emailLabel = label.text()
+      console.log(emailLabel)
+    })
+
+    // 2. Using invoke command
+    cy.get('[for="exampleInputEmail1"]').invoke('text').then(emailLabel => {
+      console.log(emailLabel)
+    })
+    cy.get('[for="exampleInputEmail1"]').invoke('text').as('emailLabel')
+    cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+    // 3. Invoke attribute value
+    cy.get('#exampleInputEmail1').invoke('attr', 'class').then(classValue => {
+      console.log(classValue)
+    })
+    cy.get('#exampleInputEmail1').should('have.attr', 'class', 'input-full-width size-medium status-basic shape-rectangle nb-transition')
+
+    // 4. Invoke input field value
+    cy.get('#exampleInputEmail1').type('hello@test.com')
+    cy.get('#exampleInputEmail1').invoke('prop', 'value').then( value => {
+      console.log(value)
+    })
   })
 });
 
