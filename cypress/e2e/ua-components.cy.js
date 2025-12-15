6ba2f5c6c3b649faff002e11b400edd7723c1e32
Inna3112 <innula3112@gmail.com>
2025-12-15 13:16:41 +0200

    web tables

--- cypress/e2e/ua-components.cy.js
+++ cypress/e2e/ua-components.cy.js
@@ -73,7 +73,7 @@ it('lists and dropdowns', () => {
   })
 })
 
-it.only('tooltips', () => {
+it('tooltips', () => {
   //tooltip - це невелике спливаюче вікно з підказкою, яке з'являється, коли користувач наводить курсор миші на певний елемент інтерфейсу.
   cy.contains('Modal & Overlays').click()
   cy.contains('Tooltip').click()
@@ -83,7 +83,7 @@ it.only('tooltips', () => {
   cy.get('nb-tooltip').should('have.text', 'This is a tooltip')
 })
 
-it.only('dialog boxes', () => {
+it('dialog boxes', () => {
   cy.contains('Tables & Data').click()
   cy.contains('Smart Table').click()
 
@@ -103,4 +103,48 @@ it.only('dialog boxes', () => {
   })
   cy.get('.nb-trash').first().click()
   cy.get('@dialogBox').should('be.calledWith', 'Are you sure you want to delete?')
+})
+
+it.only('web tables', () => {
+  cy.contains('Tables & Data').click()
+  cy.contains('Smart Table').click()
+
+  //1. How to find by text
+  cy.get('tbody').contains('tr', 'Larry').then( tableRow => {
+    cy.wrap(tableRow).find('.nb-edit').click()
+    cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('35')
+    cy.wrap(tableRow).find('.nb-checkmark').click()
+    cy.wrap(tableRow).find('td').last().should('have.text', '35')
+  })
+
+  //2. How to find by index
+  cy.get('.nb-plus').click()
+  //отримуємо thead в ньому другий tr (індексація з 0)
+  cy.get('thead tr').eq(2).then(tableRow => {
+    cy.wrap(tableRow).find('[placeholder="First Name"]').type('John')
+    cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Smith')
+    cy.wrap(tableRow).find('.nb-checkmark').click()
+  })
+
+  cy.get('tbody tr').first().find('td').then( tableColumns => {
+    cy.wrap(tableColumns).eq(2).should('have.text', 'John')
+    cy.wrap(tableColumns).eq(3).should('have.text', 'Smith')
+  })
+
+  //3. Looping though the rows
+  const ages = [20, 30, 40, 200]
+
+  cy.wrap(ages).each(age => {
+    cy.get('[placeholder="Age"]').clear().type(age)
+    //затримка щоб таблиця встигла оновитися перед перевіркою
+    //але це не найкраща практика, тому краще уникати її
+    cy.wait(500)
+    cy.get('tbody tr').each(tableRows => {
+      if (age === 200) {
+        cy.wrap(tableRows).should('contain.text', 'No data found')
+      } else {
+        cy.wrap(tableRows).find('td').last().should('have.text', age)
+      }
+    })
+  })
 })
